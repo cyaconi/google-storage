3c1028215c16b985ba0ff2bdb37e36f2a60fee29
Juris Galang <jurisgalang@gmail.com>
2010-08-29 23:20:01 -0700

    debugging

--- test-adhoc/get-bucket.rb
+++ test-adhoc/get-bucket.rb
@@ -4,19 +4,19 @@
 # Test implementation of Google Storage's GET Bucket
 # https://code.google.com/apis/storage/docs/reference-methods.html#getbucket
 #
-require 'google-storage'
+require 'lib/google-storage'
 REQUEST_DATE = Time.now.gmtime.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
 # request bucket content
 authorization = GoogleStorage::Authorization.new("test")
 uri = URI.parse('http://commondatastorage.googleapis.com/')
-req = Net::HTTP::Get.new('/kreekt-bucket-2/?delimiter=/&prefix=folder-1/')
+req = Net::HTTP::Get.new('/kreekt-bucket-2/?delimiter=/&prefix=folder-1')
 req["content-length"] = "0"
 req["content-type"]   = "application/x-www-form-urlencoded"
 req["date"]           = REQUEST_DATE
 req["user-agent"]     = "Ruby/1.8"
 req["host"]           = uri.host
-req["authorization"]  = authorization.generate(req)
+req["authorization"]  = authorization.generate(req, 'GOOG1')
 req.each do |k, v|
   puts "#{k}:\t#{v}"
 end
@@ -36,7 +36,7 @@ req["content-type"]   = "application/x-www-form-urlencoded"
 req["date"]           = REQUEST_DATE
 req["user-agent"]     = "Ruby/1.8"
 req["host"]           = uri.host
-req["authorization"]  = authorization.generate(req)
+req["authorization"]  = authorization.generate(req, 'GOOG1')
 req.each do |k, v|
   puts "#{k}:\t#{v}"
 end
