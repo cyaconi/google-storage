4ec8e752c2d0659402bbbf6468c5e22aceaa892d
Juris Galang <jgalang@sushi.gateway.2wire.net>
2010-09-10 20:24:58 -0700

    added support for params in request

--- lib/google-storage/request-methods.rb
+++ lib/google-storage/request-methods.rb
@@ -21,9 +21,10 @@ module GoogleStorage
       uri    = URI.parse("http://#{HOST}/")
       body   = options.delete(:body)
       stream = options.delete(:'body-stream')
-      path   = options.delete(:path) || uri.path
       params = options.delete(:params)
-      params.stringify_keys! unless params.nil? or params.empty?
+      params = params.map{ |k, v| "#{k}=#{v}" }.join("&") unless params.blank? 
+      path   = options.delete(:path) || uri.path
+      path << "?#{params}" unless params.blank? || path =~ /\?acl$/
       
       req = Kernel.constant("Net::HTTP::#{verb.to_s.capitalize}").new(path)
       req["content-length"] = body.nil? ? "0" : body.to_s.length
@@ -32,12 +33,10 @@ module GoogleStorage
       req["date"]           = timestamp
       req["host"]           = uri.host
       options.each{ |k, v| req[k.to_s] = v }
-      #req.each{ |k, v| puts "#{k}:\t#{v}" }
       authorize(req)
       
       req.body_stream = stream unless stream.nil?
       res = Net::HTTP.new(uri.host, uri.port).start{ |http| http.request(req, body) } 
-      # stream.close unless stream.nil?
       doc = Nokogiri::XML(res.body) 
       
       [ res, doc ]
